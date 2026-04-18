d2425f786b4e64af5099ed2f706618e92c33ab27
Sneha Sharma <snehav2109@gmail.com>
2026-04-18 16:27:47 +0530

    feat: add folder configuration in cloudiinary

--- backend/src/utils/cloudinary.ts
+++ backend/src/utils/cloudinary.ts
@@ -13,6 +13,7 @@ const uploadOnCloudinary = async (localFilePath: string) => {
     if (!localFilePath) return null;
     //upload file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
+      folder: "dobby-img-drive",
       resource_type: "auto",
     });
     //file has been uploaded successfully
@@ -33,6 +34,7 @@ const uploadRemoteToCloudinary = async (remoteUrl: string) => {
     if (!remoteUrl?.trim()) return null;
 
     return await cloudinary.uploader.upload(remoteUrl.trim(), {
+      folder: "dobby-img-drive",
       resource_type: "auto",
     });
   } catch (error) {
